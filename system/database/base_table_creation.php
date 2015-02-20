84f3da5b7815228aacfa5865ad0ae82af50074f6
Joel Klein <jdk514@gwmail.gwu.edu>
2015-02-20 11:58:28 -0500

    updated base tables, added .htacces to ignore

--- system/database/base_table_creation.php
+++ system/database/base_table_creation.php
@@ -1,5 +1,15 @@
 <?php
 include "../../application/config/environment_variables.php";
+
+//Connect to database
+echo $password;
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+
 /* Script has not been told anything, exit */
 if ($argc <= 1) {
 	echo "Please use 'destroy' or 'create' with this script to create/destroy the table";
@@ -8,31 +18,32 @@ if ($argc <= 1) {
 
 /* If script is told to destroy the tables */
 if ($argv[1] == "destroy") {
-	$sql = "DROP TABLE Students, Faculty, Forms";
+	$sql = "DROP DATABASE SEASForms";
 	if ($conn->query($sql) === TRUE) {
 	    echo "Database dropped\n";
 	} else {
 	    echo "Error dropping tables: " . $conn->error;
 	}
 	$conn->close();
-	exit();
+	exit();		
 } else if ($argv[1] == "create") {
 
-/*	$sql = "CREATE DATABASE IF NOT EXISTS {$database}";
+	$sql = "CREATE DATABASE IF NOT EXISTS SEASForms";
 	if ($conn->query($sql) === TRUE) {
 	    echo "Database created successfully\n";
 	} else {
 	    echo "Error creating database: " . $conn->error;
 	}
 
-	$conn->close();*/
+	$conn->close();
 
 	$conn = new mysqli($servername, $username, $password, $database);
 
 	$sql = "CREATE TABLE IF NOT EXISTS students (gwid VARCHAR(9) PRIMARY KEY, major VARCHAR(5), year VARCHAR(10));" .
 			"CREATE TABLE IF NOT EXISTS faculty (id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, name VARCHAR(100), major VARCHAR(5) NOT NULL);" .
 			"CREATE TABLE IF NOT EXISTS forms (id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, form_name VARCHAR(255));" .
-			"CREATE TABLE IF NOT EXISTS sub_forms;
+			"CREATE TABLE IF NOT EXISTS sub_forms (id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, fid INT FOREIGN KEY NOT NULL, sub_form_name VARCHAR(255) NOT NULL);" .
+			"CREATE TABLE IF NOT EXISTS admins (id TINYINT(4) PRIMARY KEY NOT NULL AUTO_INCREMENT, username VARCHAR(10) NOT NULL, password VARCHAR(100) NOT NULL);";
 	if ($conn->multi_query($sql) === TRUE) {
 		echo "Tables Created\n";
 		echo $conn->error;
